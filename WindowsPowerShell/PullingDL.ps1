58b6c4d75540534ba58dced91f21d02adb0feb84
Heng Yon <heng.yon@avanade.com>
2016-12-12 14:12:25 -0800

    Pullin DLNames

---
+++ WindowsPowerShell/PullingDL.ps1
@@ -0,0 +1,13 @@
+﻿$DLNames =@("itshostadcs", "itshostadops", "DBARole", "itssqleng", "ITSHostSQLOps" )
+$Allusers = @()
+$date = get-date -Format ("yyyy/MM/dd")
+
+Foreach($DLName in $DLNames){
+ $users = get-adgroupmember $DLName -recursive | select SamAccountName 
+ $users |   Add-Member -Name  Group -Value $DLName -MemberType NoteProperty 
+ $users | add-member -name Date -value  $date -MemberType NoteProperty 
+ #$allUsers = $allusers +  $users - use shorthand
+ $allUsers += $users
+
+}
+$Allusers | export-csv Audits.csv -NoTypeInformation
